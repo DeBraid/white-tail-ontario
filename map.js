15cb4e64bf2fae71b63754572e630d37bf701b44
DeBraid <de.braid@gmail.com>
2020-02-02 13:27:39 -0500

    async data tentatively working

--- map.js
+++ map.js
@@ -40,7 +40,6 @@ const whiteTailData = async () => {
 
 const whiteTailToMapFeatures = (map_data, white_tails) => {
   const features = map_data.features;
-  // TODO: need to merge harvest values with features
   features.map(feature => {
     const { WMU } = feature.properties;
     white_tails.map((whitetail_row) => {
@@ -48,56 +47,29 @@ const whiteTailToMapFeatures = (map_data, white_tails) => {
         feature.whitetail_hunting_data = Number(whitetail_row.Total_Harvest)
       }
     });
-    // d3.csv('white-tailed_deer_2019.csv').then((whitetails) => {
-    // });
   });
   return features;
 }
 
 const fetchData = async () => {
   const [a, b] = await Promise.all([geoJson, whiteTailData])
-  // console.log('fetchData a', a());
-  // console.log('fetchData b', b());
   const mapData = await a();
   const whiteTailRaw = await b();
-  console.log('fetchData mapData.features', mapData.features);
-  console.log('fetchData whiteTailRaw', whiteTailRaw);
-
-  const baz = whiteTailToMapFeatures(mapData, whiteTailRaw);
-  console.log('baz', baz);
+  return await whiteTailToMapFeatures(mapData, whiteTailRaw);
 }
 
-fetchData();
-
-d3.json(geoJsonPath)
-
-.then((mapData) => {
-  const features = mapData.features;
-  // TODO: need to merge harvest values with features
-  features.map(feature => {
-    const { WMU } = feature.properties;
-    // feature.whitetail_hunting_data = [];
-    d3.csv('white-tailed_deer_2019.csv').then((whitetails) => {
-      whitetails.map((whitetail_row) => {
-        if (WMU == whitetail_row.WMU) {
-          feature.whitetail_hunting_data = Number(whitetail_row.Total_Harvest)
-        }
-      });
-    });
-  });
-  return features;
-})
-.then((features) => {
-
-  console.log('features', features);
-  // Set constants for color scale
+const drawChart = async () => {
+  const features = await fetchData();
+  console.log('drawChart features', features);
   const minDomain = 0;
   // const whitetailsArr = features.map(f => {
   //   // console.log('f', f);
   //   f.whitetail_hunting_data
   // });
   // console.log('whitetailsArr', whitetailsArr);
-  const maxDomain = features.length;
+
+  // const maxDomain = features.length;
+  const maxDomain = 5000;
   const colorRange = ["#f2ffe6","#59b300"];
 
   // Color and Fill functions
@@ -105,9 +77,7 @@ d3.json(geoJsonPath)
     .domain([minDomain, maxDomain])
     .range(colorRange);
 
-  const fillFunction = (d, i) => {
-    return color(d.whitetail_hunting_data);
-  };
+  const fillFunction = (d, i) => color(d.whitetail_hunting_data);
 
   // Add the polygons
   const polygons = map.selectAll('path')
@@ -135,4 +105,75 @@ d3.json(geoJsonPath)
     d3.select(this).attr('fill', fillFunction(d,i));
     featureName.text('');
   }
-});
+}
+
+drawChart();
+
+// d3.json(geoJsonPath)
+
+// .then((mapData) => {
+//   const features = mapData.features;
+//   // TODO: need to merge harvest values with features
+//   features.map(feature => {
+//     const { WMU } = feature.properties;
+//     // feature.whitetail_hunting_data = [];
+//     d3.csv('white-tailed_deer_2019.csv').then((whitetails) => {
+//       whitetails.map((whitetail_row) => {
+//         if (WMU == whitetail_row.WMU) {
+//           feature.whitetail_hunting_data = Number(whitetail_row.Total_Harvest)
+//         }
+//       });
+//     });
+//   });
+//   return features;
+// })
+
+// .then((features) => {
+
+//   console.log('features', features);
+//   // Set constants for color scale
+//   const minDomain = 0;
+//   // const whitetailsArr = features.map(f => {
+//   //   // console.log('f', f);
+//   //   f.whitetail_hunting_data
+//   // });
+//   // console.log('whitetailsArr', whitetailsArr);
+//   const maxDomain = features.length;
+//   const colorRange = ["#f2ffe6","#59b300"];
+
+//   // Color and Fill functions
+//   const color = d3.scaleLinear()
+//     .domain([minDomain, maxDomain])
+//     .range(colorRange);
+
+//   const fillFunction = (d, i) => {
+//     return color(d.whitetail_hunting_data);
+//   };
+
+//   // Add the polygons
+//   const polygons = map.selectAll('path')
+//     .data(features)
+//     .enter()
+//     .append('path')
+//     .attr('d', path)
+//     .attr("fill", fillFunction)
+//     .attr("fill-opacity", 0.5)
+//     .attr("stroke", "#222")
+//     .on('mouseover', onMouseOver)
+//     .on('mouseout', onMouseOut);
+
+//   // OnMouseOver function
+//   function onMouseOver(d, i) {
+//     console.log('onMouseOver whitetail_hunting_data', d.whitetail_hunting_data);
+//     d3.select(this).attr('fill', '#ffcb6b');
+//     const featureText = d.properties.WMU + ' - ' + Math.round(d.properties.SYS_AREA/1000000) + '   KM²';
+
+//     featureName.text(featureText);
+//   }
+
+//   // OnMouseOut function
+//   function onMouseOut(d, i) {
+//     d3.select(this).attr('fill', fillFunction(d,i));
+//     featureName.text('');
+//   }
+// });
